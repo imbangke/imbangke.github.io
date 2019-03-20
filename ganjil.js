43c7034ca035e44db0a8d649d523c80f936dcc06
imbangke <48753413+imbangke@users.noreply.github.com>
2019-03-20 16:46:01 +0700

    Create ganjil.js

---
+++ ganjil.js
@@ -0,0 +1,11 @@
+let url1 = [
+              'https://facebook.com1',
+              'https://github.com1'
+
+            ];
+ //genap
+  let url2 = [
+              'https://facebook.com2',
+              'https://github.com2'
+
+            ];
