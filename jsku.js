5824eadbb3f63a1c2cf7933183bfc3caf073bc20
unknown <imambangke@yahoo.com>
2019-03-24 15:16:00 +0700

    baru

--- jsku.js
+++ jsku.js
@@ -4,6 +4,7 @@ let day = date.getDate();
 let jam = date.getHours();
 
 
+
 //=========================== list
 
   let urla = [
@@ -16,7 +17,12 @@ let jam = date.getHours();
     'https://www.youtube.com/watch?v=ofdQdDRgNUk',
     'https://www.dailymotion.com/video/x3ltc0b',
     'https://www.youtube.com/watch?v=jj3GiN0q4Ow',
-    'https://www.dailymotion.com/video/x2i9ke7'
+    'https://www.dailymotion.com/video/x2i9ke7',
+	'https://www.youtube.com/watch?v=EAg4wNVvC-8',
+	'https://www.youtube.com/watch?v=EX1I2LI8Wc8',
+	'https://www.youtube.com/watch?v=hwsDPcoKY6M',
+	'https://www.youtube.com/watch?v=ohKbDiT1dCQ',
+
 
             ];
 
@@ -29,7 +35,14 @@ let jam = date.getHours();
     'https://www.youtube.com/watch?v=QDr81GTND9Y',
     'https://www.dailymotion.com/video/x1trcym',
     'https://www.youtube.com/watch?v=8aLzmoeWFgI',
-    'https://www.youtube.com/watch?v=x-A1ohpgdlM'
+    'https://www.youtube.com/watch?v=x-A1ohpgdlM',
+'https://www.youtube.com/watch?v=0lkEoAnZSR8',
+'https://www.youtube.com/watch?v=G8jQKg2wG28',
+'https://www.youtube.com/watch?v=qH3tfL966Kg',
+'https://www.youtube.com/watch?v=06ur0GipKwA',
+'https://www.youtube.com/watch?v=yiveCA9h8T4',
+'https://www.youtube.com/watch?v=O2ulyJuvU3Q',
+'https://www.youtube.com/watch?v=nMfPqeZjc2c',
 
             ];
 
@@ -43,29 +56,451 @@ let jam = date.getHours();
     'https://www.dailymotion.com/video/x5bwxl2',
     'https://www.youtube.com/watch?v=jOY1sjlJ6A8',
     'https://www.dailymotion.com/video/x47bqk3',
-    'https://www.youtube.com/watch?v=gmU9PBDS-0k'
+    'https://www.youtube.com/watch?v=gmU9PBDS-0k',
+'https://www.youtube.com/watch?v=ZYnsuCt3vLI',
+'https://www.youtube.com/watch?v=D0i23wZ0IeE',
+'https://www.youtube.com/watch?v=g4mHPeMGTJM',
+'https://www.youtube.com/watch?v=43t324f5w-c',
+'https://www.youtube.com/watch?v=IkdmOVejUlI',
+'https://www.youtube.com/watch?v=3yOqnyaisMc',
+'https://www.youtube.com/watch?v=u_YgyKW4nA4',
+
+
+            ];
+			
+	 let urld = [
+	 
+			'https://www.youtube.com/watch?v=Fm0sToWtatw',
+'https://www.youtube.com/watch?v=WpxJJFcWM8s',
+'https://www.youtube.com/watch?v=vplX-qr4AIE',
+'https://www.youtube.com/watch?v=fES1VjPgFHc',
+'https://www.youtube.com/watch?v=tcgC9Y0e00w',
+'https://www.youtube.com/watch?v=82D56u8zeWY',
+'https://www.youtube.com/watch?v=-i8IMw5WeXc',
+'https://www.youtube.com/watch?v=Jrg9E2YBgsI',
+'https://www.youtube.com/watch?v=BN7QE1ZFGGk',
+'https://www.youtube.com/watch?v=dWoIljGEOQ4',
+'https://www.youtube.com/watch?v=XSL1HbrE3DE',
+'https://www.youtube.com/watch?v=gVKEM4K8J8A',
+'https://www.youtube.com/watch?v=Hp4TjOqsVcs',
+'https://www.youtube.com/watch?v=2R2gb0MKJlo',
+'https://www.youtube.com/watch?v=hGlyFc79BUE',
+'https://www.youtube.com/watch?v=d1RzilWcfVs',
+'https://www.youtube.com/watch?v=MsfbGmctqLM',
+'https://www.youtube.com/watch?v=z9Uz1icjwrM',
+    
+
+            ];
+			
+	
+	let urlf = [
+    
+			
+'https://www.youtube.com/watch?v=wpU4E63Hj7k',
+'https://www.youtube.com/watch?v=nRBCFsU_KDc',
+'https://www.youtube.com/watch?v=FzFqlfoHw_4',
+'https://www.youtube.com/watch?v=UhZOsmO9JMs',
+'https://www.youtube.com/watch?v=3sL0omwElxw',
+'https://www.youtube.com/watch?v=oHBNDmwhFtY',
+'https://www.youtube.com/watch?v=kH-kj6rkQWc',
+'https://www.youtube.com/watch?v=kygoJiJAWG4',
+'https://www.youtube.com/watch?v=6ddO3jPUFpg',
+'https://www.youtube.com/watch?v=wq6T8L5_Dps',
+'https://www.youtube.com/watch?v=DmTk5dwhays',
+'https://www.youtube.com/watch?v=sCgqiOzXE-g',
+'https://www.youtube.com/watch?v=dm22MN2E3jI',
+'https://www.youtube.com/watch?v=uSaXRqCU464',
+'https://www.youtube.com/watch?v=iqT0iFZifgw',
+'https://www.youtube.com/watch?v=fIgm6lxvUfQ',
+'https://www.youtube.com/watch?v=OE9bF80KQGk',
+'https://www.youtube.com/watch?v=kqS1vsF5gQQ',
+'https://www.youtube.com/watch?v=5OgILZbLNQ0',
+
+            ];
+			
+			
+			
+	let urlg = [
+	
+'https://www.youtube.com/watch?v=IN3891Aqam4',
+'https://www.youtube.com/watch?v=uQqDfIMsUhU',
+'https://www.youtube.com/watch?v=NBZn07IWjbQ',
+'https://www.youtube.com/watch?v=RpX7uAuDSbE',
+'https://www.youtube.com/watch?v=Qo4JIT8jMtI',
+'https://www.youtube.com/watch?v=lZOyh_OS_aw',
+'https://www.youtube.com/watch?v=1CSPVkMgiTs',
+'https://www.youtube.com/watch?v=sARlCTIx3TU',
+'https://www.youtube.com/watch?v=_LVfsrS7fUk',
+'https://www.youtube.com/watch?v=Mk1IEl1cjVY',
+'https://www.youtube.com/watch?v=9E3zNHd936c',
+'https://www.youtube.com/watch?v=7ev8I2AW9ko',
+'https://www.youtube.com/watch?v=yw1Du5rl3Bg',
+'https://www.youtube.com/watch?v=sR8oMMRm_IE',
+'https://www.youtube.com/watch?v=Kz15Eg36rnY',
+'https://www.youtube.com/watch?v=XIMLoLxmTDw',
+'https://www.youtube.com/watch?v=WKB0JUkksJg',
+'https://www.youtube.com/watch?v=8GC57huXLYY',
+'https://www.youtube.com/watch?v=epYR4Bb3G7w',
+'https://www.youtube.com/watch?v=kxopViU98Xo',
+'https://www.youtube.com/watch?v=8CdcCD5V-d8',
+'https://www.youtube.com/watch?v=xrENmPeHUbc',
+    
+
+            ];
+			
+			
+	let urlh = [
+	
+			
+'https://www.youtube.com/watch?v=Sv0LwXYAVVg',
+'https://www.youtube.com/watch?v=WvRv-243Cmk',
+'https://www.youtube.com/watch?v=enDmlfIkWe8',
+'https://www.youtube.com/watch?v=pj1jHGU4a4g',
+'https://www.youtube.com/watch?v=3VqeepWaI1g',
+'https://www.youtube.com/watch?v=SR0PGXgUqms',
+'https://www.youtube.com/watch?v=9uIk_91GQYI',
+'https://www.youtube.com/watch?v=SJww4kcJas4',
+'https://www.youtube.com/watch?v=NxqlEXP3A3g',
+'https://www.youtube.com/watch?v=PErqizZqLjI',
+'https://www.youtube.com/watch?v=l3N--MZJIx4',
+'https://www.youtube.com/watch?v=MMdjFaIVulg',
+'https://www.youtube.com/watch?v=Qm846KdZN_c',
+'https://www.youtube.com/watch?v=Uq7kyf1T_lk',
+'https://www.youtube.com/watch?v=2Q1EDFA0U2s',
+'https://www.youtube.com/watch?v=k1hgJkIGjUg',
+'https://www.youtube.com/watch?v=vs1mXnWuHd8',
+    
+
+            ];
+			
+			
+	let urli = [
+	
+			'https://www.youtube.com/watch?v=hsGOT_0L16U',
+'https://www.youtube.com/watch?v=BlE-hfPeMN8',
+'https://www.youtube.com/watch?v=QZbuj3RJcjI',
+'https://www.youtube.com/watch?v=o8GrqUSdzi0',
+'https://www.youtube.com/watch?v=Uk6-t7CbnrE',
+'https://www.youtube.com/watch?v=hlWiI4xVXKY',
+'https://www.youtube.com/watch?v=4wtG01ymIjE',
+'https://www.youtube.com/watch?v=oUmqsetmQyw',
+'https://www.youtube.com/watch?v=v8JRzXCFHrw',
+'https://www.youtube.com/watch?v=YV-5SqpjBII',
+'https://www.youtube.com/watch?v=NuIAYHVeFYs',
+'https://www.youtube.com/watch?v=qrx1vyvtRLY',
+'https://www.youtube.com/watch?v=1ZYbU82GVz4',
+'https://www.youtube.com/watch?v=J4sGK1tGJ8s',
+'https://www.youtube.com/watch?v=LH-AfaJ3yAU',
+'https://www.youtube.com/watch?v=g1N2UD5ScGA',
+'https://www.youtube.com/watch?v=8wwFGUEAGw8',
+'https://www.youtube.com/watch?v=3wRO9ymqxYg',
+'https://www.youtube.com/watch?v=SVgvf0a2fpg',
+    
+
+            ];
+			
+			
+	let urlj = [
+    
+		
+'https://www.youtube.com/watch?v=xTd3gAfIa7k',
+'https://www.youtube.com/watch?v=ujyu7tkyxtw',
+'https://www.youtube.com/watch?v=AQIJaj4bdgg',
+'https://www.youtube.com/watch?v=-aM_84Vlw0g',
+'https://www.youtube.com/watch?v=Lz41-JtVgOg',
+'https://www.youtube.com/watch?v=x5zGL9q8BZ0',
+'https://www.youtube.com/watch?v=Cnfj6QCGLyA',
+'https://www.youtube.com/watch?v=eKFTSSKCzWA',
+'https://www.youtube.com/watch?v=lFcSrYw-ARY',
+'https://www.youtube.com/watch?v=VGymB7yUlYI',
+'https://www.youtube.com/watch?v=6ixhN9umyp4',
+'https://www.youtube.com/watch?v=FfdIpDNV_TQ',
+'https://www.youtube.com/watch?v=xMyoy_HNDPE',
+'https://www.youtube.com/watch?v=nTY_2MjQ2CI',
+'https://www.youtube.com/watch?v=G2JcmOm5HqU',
+'https://www.youtube.com/watch?v=-L2aB6ptel8',
+'https://www.youtube.com/watch?v=wzjWIxXBs_s',
+'https://www.youtube.com/watch?v=ZdX_X_jOsyE',
+'https://www.youtube.com/watch?v=VcsEA0zl8LU',
+'https://www.youtube.com/watch?v=y7e-GC6oGhg',
+'https://www.youtube.com/watch?v=pnD76ZLWEz8',
+'https://www.youtube.com/watch?v=OCa6O-oMcNY',
 
             ];
 
+	let urlk = [
+	
+		'https://www.youtube.com/watch?v=ORh-1PRY6EA',
+'https://www.youtube.com/watch?v=xQ6xgDI7Whc',
+'https://www.youtube.com/watch?v=QASbw8_0meM',
+'https://www.youtube.com/watch?v=AgpWX18dby4',
+'https://www.youtube.com/watch?v=-50NdPawLVY',
+'https://www.youtube.com/watch?v=L_LUpnjgPso',
+'https://www.youtube.com/watch?v=jX6kn9_U8qk',
+'https://www.youtube.com/watch?v=lasWefVUCsI',
+'https://www.youtube.com/watch?v=wI__53kBBKM',
+'https://www.youtube.com/watch?v=dN1XUV6QclU',
+'https://www.youtube.com/watch?v=nDq6TstdEi8',
+'https://www.youtube.com/watch?v=f77SKdyn-1Y',
+'https://www.youtube.com/watch?v=EbnH3VHzhu8',
+'https://www.youtube.com/watch?v=wJpQ5qusSzc',
+'https://www.youtube.com/watch?v=e4CQHzLAi8o',
+'https://www.youtube.com/watch?v=gmU9PBDS-0k',
+'https://www.youtube.com/watch?v=gpvznAiKblU',
+'https://www.youtube.com/watch?v=so-SG1d4o3o',
+'https://www.youtube.com/watch?v=_5LBU9dUhF0',
+'https://www.youtube.com/watch?v=3BS5uGSwKwA',
+'https://www.youtube.com/watch?v=WbEGmghn_jo',
+'https://www.youtube.com/watch?v=xWcldHxHFpo',
+'https://www.youtube.com/watch?v=eBPfnj8_4W4',
+    
+
+            ];
+			
+			
+			let urll = [
+	
+				
+'https://www.youtube.com/watch?v=WBu2pJpgKEo',
+'https://www.youtube.com/watch?v=7bkk1ibiPRw',
+'https://www.youtube.com/watch?v=a56kTSFpJBI',
+'https://www.youtube.com/watch?v=77ZozI0rw7w',
+'https://www.youtube.com/watch?v=JySwxTeQqsw',
+'https://www.youtube.com/watch?v=G1IbRujko-A',
+'https://www.youtube.com/watch?v=v5bClGpH0iI',
+'https://www.youtube.com/watch?v=o8GrqUSdzi0',
+'https://www.youtube.com/watch?v=aDfZ6STAfqA',
+'https://www.youtube.com/watch?v=C5Gm8UvxKlU',
+'https://www.youtube.com/watch?v=aJaZc4E8Y4U',
+'https://www.youtube.com/watch?v=KgbSjRMqyjc',
+'https://www.youtube.com/watch?v=DYgqNZ349Ow',
+'https://www.youtube.com/watch?v=skRQZ9yVNgY',
+'https://www.youtube.com/watch?v=c1XOgrBz6sU',
+'https://www.youtube.com/watch?v=1kJqGGsJ_os',
+'https://www.youtube.com/watch?v=wnhvanMdx4s',
+'https://www.youtube.com/watch?v=X5zWUYEkgXM',
+'https://www.youtube.com/watch?v=v7l8sh7ErRc',
+'https://www.youtube.com/watch?v=gQs4B7OWlhs',
+'https://www.youtube.com/watch?v=ZBECpvELkAM',
+'https://www.youtube.com/watch?v=NI_fgwbmJg0',
+		
+    
+
+            ];
+			
+			
+			let urlm = [
+	
+			
+'https://www.youtube.com/watch?v=nMAzchVWTis',
+'https://www.youtube.com/watch?v=oewj_XEM1js',
+'https://www.youtube.com/watch?v=wZZ7oFKsKzY',
+'https://www.youtube.com/watch?v=wqd81s0vrAQ',
+'https://www.youtube.com/watch?v=60mLvBWOMb4',
+'https://www.youtube.com/watch?v=UcRtFYAz2Yo',
+'https://www.youtube.com/watch?v=t_S_cN2re4g',
+'https://www.youtube.com/watch?v=fyo54_v21lM',
+'https://www.youtube.com/watch?v=M5V9CERbLcA',
+'https://www.youtube.com/watch?v=guo8CHurCpY',
+'https://www.youtube.com/watch?v=a_GJ1k8-zNo',
+'https://www.youtube.com/watch?v=Kob0G2hE8IY',
+'https://www.youtube.com/watch?v=AS5CxLCWq-Q',
+'https://www.youtube.com/watch?v=_RPjndlY-sc',
+'https://www.youtube.com/watch?v=UCDxZz6R1h0',
+'https://www.youtube.com/watch?v=T75IKSXVXlc',
+'https://www.youtube.com/watch?v=g9c2WTCj0Pk',
+'https://www.youtube.com/watch?v=s9q61m7S9yA',
+'https://www.youtube.com/watch?v=tolumIARJfM',
+'https://www.youtube.com/watch?v=wXMD6wmcwvE',
+'https://www.youtube.com/watch?v=T2RaehsIbkU',
+'https://www.youtube.com/watch?v=uNFEecf-4oI',
+		
+    
+
+            ];
+			
+			
+			let urln = [
+	
+	
+			
+'https://www.youtube.com/watch?v=lZOyh_OS_aw',
+'https://www.youtube.com/watch?v=2zSR_53DDv0',
+'https://www.youtube.com/watch?v=gfkts0u-m6w',
+'https://www.youtube.com/watch?v=y98De45A94Y',
+'https://www.youtube.com/watch?v=JNFX32Hz_N0',
+'https://www.youtube.com/watch?v=TRc85qoNo6w',
+'https://www.youtube.com/watch?v=8plwv25NYRo',
+'https://www.youtube.com/watch?v=-PWugcP3CfM',
+'https://www.youtube.com/watch?v=jbAy9MwBR-I',
+'https://www.youtube.com/watch?v=v6mHKcdwpwY',
+'https://www.youtube.com/watch?v=51skM6WA-dg',
+'https://www.youtube.com/watch?v=fgcHhtQkYTs',
+'https://www.youtube.com/watch?v=iJ7jKIc3eQo',
+'https://www.youtube.com/watch?v=9C9aJgiEOBg',
+'https://www.youtube.com/watch?v=TpReQ1XXQzs',
+'https://www.youtube.com/watch?v=FkbU6JOCit0',
+'https://www.youtube.com/watch?v=UleQ9ZKgXI8',
+'https://www.youtube.com/watch?v=ifgkS82DUlg',
+'https://www.youtube.com/watch?v=hdcSrCUbjoE',
+'https://www.youtube.com/watch?v=OWFBqiUgspg',
+'https://www.youtube.com/watch?v=Wyq7axTnjO4',
+'https://www.youtube.com/watch?v=y5zQTmkY7GI'
+		
+    
+
+            ];
+			
+			
+			let urlo = [
+	
+'https://www.youtube.com/watch?v=ClPo_XZnmr4',
+'https://www.youtube.com/watch?v=0i-fIMLIm8g',
+'https://www.youtube.com/watch?v=8t8GJwRDELM',
+'https://www.youtube.com/watch?v=UZvF9a0aNys',
+'https://www.youtube.com/watch?v=3acYFiDejfA',
+'https://www.youtube.com/watch?v=PC1XCR8r8sY',
+'https://www.youtube.com/watch?v=oQY_xAmywxk',
+'https://www.youtube.com/watch?v=eLjSyI8bNfk',
+'https://www.youtube.com/watch?v=OB2ekdVqPnk',
+'https://www.youtube.com/watch?v=oXp0hTkXiks',
+'https://www.youtube.com/watch?v=ZiaDy_4GLdk',
+'https://www.youtube.com/watch?v=u3pQv9mmZMk',
+'https://www.youtube.com/watch?v=sCNrK-n68CM',
+'https://www.youtube.com/watch?v=lqrJVVIwjzE',
+'https://www.youtube.com/watch?v=Zl-yRnZIULw',
+'https://www.youtube.com/watch?v=vPmjgHrVZYg',
+'https://www.youtube.com/watch?v=PaXTSFKQR3Y',
+'https://www.youtube.com/watch?v=PMtNRXu3z54',
+'https://www.youtube.com/watch?v=eJT8xuI_5PY',
+'https://www.youtube.com/watch?v=xAwB9lQnxAY',
+'https://www.youtube.com/watch?v=09m0B8RRiEE',
+'https://www.youtube.com/watch?v=wKhFZPefb64',
+'https://www.youtube.com/watch?v=NMCg2Q8eRyI',
+	
+	
+    
+
+            ];
+			
+			
+			let urlp = [
+	
+		'https://www.youtube.com/watch?v=fIUfkbvYz88',
+'https://www.youtube.com/watch?v=dhCeseXZEKA',
+'https://www.youtube.com/watch?v=Lmq1us8EX_0',
+'https://www.youtube.com/watch?v=kB2L6nU8bFQ',
+'https://www.youtube.com/watch?v=_8mZXyLBVx0',
+'https://www.youtube.com/watch?v=1Zu8xfsJkNQ',
+'https://www.youtube.com/watch?v=GIBmfSBZyiA',
+'https://www.youtube.com/watch?v=Ym9saCWYaiM',
+'https://www.youtube.com/watch?v=1SijELJNfrI',
+'https://www.youtube.com/watch?v=wbby9coDRCk',
+'https://www.youtube.com/watch?v=v8JRzXCFHrw',
+'https://www.youtube.com/watch?v=-Q_UUzftEok',
+'https://www.youtube.com/watch?v=7sbWhBlQDJ4',
+'https://www.youtube.com/watch?v=BFiuPXW01k0',
+'https://www.youtube.com/watch?v=g9rZOuTWHIk',
+'https://www.youtube.com/watch?v=33p5voIx6Dw',
+'https://www.youtube.com/watch?v=rpbVJ1XATPw',
+'https://www.youtube.com/watch?v=Go4YMAws6BU',
+'https://www.youtube.com/watch?v=OdIJ2x3nxzQ',
+'https://www.youtube.com/watch?v=ir6nk2zrMG0',
+'https://www.youtube.com/watch?v=NHEaYbDWyQE',
+'https://www.youtube.com/watch?v=7F-F8-qHmq0',
+'https://www.youtube.com/watch?v=bbjMDgMz504',
+'https://www.youtube.com/watch?v=OLg1dJKkxm8',
+'https://www.youtube.com/watch?v=HFsKxbrZKNQ',
+'https://www.youtube.com/watch?v=XrDUTpeGW0A',
+'https://www.youtube.com/watch?v=16G-Hv23nqw',
+	
+    
+
+            ];
+			
+			
+			let urlq = [
+	
+			
+'https://www.youtube.com/watch?v=vpdcMZnYCko',
+'https://www.youtube.com/watch?v=BrG8UDDbRmc',
+'https://www.youtube.com/watch?v=ZbUsCO9_Kus',
+'https://www.youtube.com/watch?v=rjKyHNtJKNk',
+'https://www.youtube.com/watch?v=VUArb3AIpm4',
+'https://www.youtube.com/watch?v=WHPEKLQID4U',
+'https://www.youtube.com/watch?v=aNtMEKkfIMI',
+'https://www.youtube.com/watch?v=qYnA9wWFHLI',
+'https://www.youtube.com/watch?v=iyoI2nlfZvE',
+'https://www.youtube.com/watch?v=KBfnACUIDWA',
+'https://www.youtube.com/watch?v=xTElIdBy3vY',
+'https://www.youtube.com/watch?v=GJ8qSYZ2vRM',
+'https://www.youtube.com/watch?v=DkBZ-GUgxco',
+'https://www.youtube.com/watch?v=qsOUv9EzKsg',
+'https://www.youtube.com/watch?v=tP0zE1zXTVA',
+'https://www.youtube.com/watch?v=mQ9OWMsJBTk',
+'https://www.youtube.com/watch?v=Yi0bvHCTQUA',
+'https://www.youtube.com/watch?v=0bNaXImL_58',
+'https://www.youtube.com/watch?v=0vbd_LfKGH0',
+'https://www.youtube.com/watch?v=M91VPs47jZc',
+	
+	
+    
+
+            ];
+			
+			
     //==========================================================================
-
-            if(day == 1  || day == 4 || day == 7 || day == 10 || day == 16 || day == 19 || day ==  22 || day == 25 || day == 28 ) {
-              if(jam == 1 || jam == 2 || jam == 3 || jam == 4 ) {
-               load = urla;
-              //console.log(load[0])
+		if(day == 1 || day == 18   ) {
+			  load = urla
+              
+            }else if(day == 2 || day == 19   ) {
+			  load = urlb
+              
+            }else if(day == 3 || day == 20   ) {
+			  load = urlc
+              
+            }else if(day == 4 || day == 21   ) {
+			  load = urld
+              
+            }else if(day == 5 || day == 22   ) {
+			  load = urle
+              
+            }else if(day == 6 || day == 23   ) {
+			  load = urlf
+              
+            }else if(day == 7 || day == 24   ) {
+			  load = urlg
+              
+            }else if(day == 8 || day == 24   ) {
+			  load = urlh
+              
+            }else if(day == 9 || day == 26   ) {
+			  load = urli
+              
+            }else if(day == 10 || day == 27   ) {
+			  load = urlj
+              
+            }else if(day == 11 || day == 28   ) {
+			  load = urlk
+              
+            }else if(day == 12 || day == 29   ) {
+			  load = urll
+              
+            }else if(day == 13 || day == 30   ) {
+			  load = urlm
+              
+            }else if(day == 14 || day == 31   ) {
+			  load = urln
+              
+            }else if(day == 15  ) {
+			  load = urlo
+              
+            }else if(day == 16 ) {
+			  load = urlp
+              
+            }else if(day == 17 ) {
+			  load = urlq
+              
             }
-            }else if(day == 2 || day == 5 ||day == 8 || day == 11 || day == 14 || day == 17 || day == 20 || day == 23 || day == 26|| day == 29 ) {
-               if(jam == 1 || jam == 2 || jam == 3 || jam == 4 ) {
-                  load = urlb
-                // console.log(load[0])
-              }
-            }else if (day == 3 || day == 6 || day == 9 || day == 12 ||day == 15 || day == 18 || day == 21 ||day == 24 || day == 27 || day == 30 ) {
-              if(jam == 1 || jam == 2 || jam == 3 || jam == 4 ) {
-               load = urlc
-              // console.log(load[0])
-              }
-            }
 
-
-//console.log(load[0])
   //===============================================================================
